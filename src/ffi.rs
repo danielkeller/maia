6327c797b0eb5207a8ff96162b2c9a74bff0d988
Daniel Keller <redink@google.com>
2022-07-01 10:20:56 +0200

    add license header

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,3 +1,11 @@
+// Copyright 2022 Google LLC
+
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 pub(crate) use std::ffi::c_void;
 use std::ffi::CStr;
 use std::fmt::Debug;
--- src/macos_instructions.rs
+++ src/macos_instructions.rs
@@ -1,3 +1,11 @@
+// Copyright 2022 Google LLC
+
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //! # MacOS-specific Instructions
 //! Bundling a Vulkan .app for MacOS requires a few extra steps. The .app bundle is really just a folder, with a structure like
 //!
